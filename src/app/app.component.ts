b51ffff0093f4801cf4e936052c6224ef0529397
Fernando Diaz M <yuloneitor@hotmail.com>
2018-07-26 14:48:57 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  valor1 = false; 
+  Descripcion = 'Pasos para resolver el ejercicio por metodo de Cramer.';
+  Paso1 = '1. Hallamos el determinante';
+
+  d = determinante();
+
+  Paso2 = '2. Hallamos X, Y, Z';
+
+  x = HallarX();
+  y = HallarY();
+  z = HallarZ();
+}
+
+function determinante() {
+  var D = 0.3 * 1 * 0.5 + 0.5 * 0.3 * 1 + 0.1 * 0.52 * 1.93 - 1 * 1 * 0.1 - 1.93 * 0.3 * 0.3 - 0.5 * 0.52 * 0.5;
+  var de = D.toFixed(4); // Limitar numeros decimales
+  return de;
+}
+
+function HallarX() {
+  var x = (-0.01 * 1 * 0.5 + 0.67 * 0.3 * 1 + -0.44 * 0.52 * 1.93 - 1 * 1 * -0.44 - 1.93 * 0.3 * -0.01 - 0.5 * 0.52 * 0.67) / 0.0022
+  var xe = x.toFixed(1);
+  return xe;
+}
+
+function HallarY() {
+  var y = (0.3 * 0.67 * 0.5 + 0.5 * -0.44 * 1 + 0.1 * -0.01 * 1.93 - 1 * 0.67 * 0.1 - 1.93 * -0.44 * 0.3 - 0.5 * -0.01 * 0.5) / 0.0022
+  var ye = y.toFixed(1); // Limitar numeros decimales
+  return ye;
+}
+
+function HallarZ() {
+  var z = (0.3 * 1 * -0.44 + 0.5 * 0.3 * -0.01 + 0.1 * 0.52 * 0.67 - (-0.01 * 1 * 0.1) - 0.67 * 0.3 * 0.3 - (-0.44 * 0.52 * 0.5)) / 0.0022
+  var ze = z.toFixed(1); // Limitar numeros decimales
+  return ze;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { MatToolbarModule } from "@angular/material";
+import {MatChipsModule} from '@angular/material/chips';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatChipsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatCardModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
